63c058690c6f2a72a4bd1cfecfd0773bc9f40e6c
Kayla <ktopper24@gmail.com>
2015-08-30 19:39:46 -0400

    Adding initial website files

---
+++ javascript.js
@@ -0,0 +1,12 @@
+
+//$(document).ready(function() {
+//  $("thumbnail-1").click(function() {
+//    $("sweet-menu").toggle();
+//  });
+//});
+
+$(document).ready(function(){
+  $("heading").click(function(){
+    $(this).hide();
+  });
+});
