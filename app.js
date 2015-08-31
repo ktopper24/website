fd14c50d8fa7d04e1a922f7c86a597dca187b82b
Justin Senkus <jsenkus@gmail.com>
2015-08-30 21:21:19 -0400

    Project Restructure + Added Node

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/',function(req,res){
+  res.sendfile("index.html");
+});
+
+app.listen(3000,function(){
+  console.log("Started on PORT 3000");
+})
